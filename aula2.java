9d74b3ee1d8fc2052e936364379ff4d96ecf08fd
✧˖°.Xorginho✧˖° <144127740+xorginhoo@users.noreply.github.com>
2024-03-19 11:22:25 -0300

    Add files via upload

---
+++ aula2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+    Scanner teclado = new scanner (System.in);
+    System.out.println("cadastre o produto");
+    String produto=teclado.next();
+    System.out.println("informe a quantidade");
+    int Quantidade=teclado.next.Int();
+    System.out.println("Informe o preço")
+    float preço=teclado.nextFloat();
+    teclado.close();
+
+}
